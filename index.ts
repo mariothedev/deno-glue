ae7dceedabd76ac4e004e8a98aa9fa7bc6077653
mariothedev <mariothedev@gmail.com>
2022-10-21 10:05:24 +0200

    Make small change

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
 serve((req: Request) => new Response("Hello World"));
+serve((req: Request) => {
+  return new Response("Hell world", {
+    headers: { "content-type": "text/plain" },
+  });
+});
