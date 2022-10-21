162447d189b6d33e232f8c6e3c45256384b41252
mariothedev <mariothedev@gmail.com>
2022-10-21 13:58:48 +0200

    change status

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
+// @ts-nocheck
+import file from "./data.json" assert { type: "json"}
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import { either } from "https://cdn.pika.dev/fp-ts@^2.4.4";
-import lodash from "https://cdn.skypack.dev/lodash";
+import { pipe } from "https://cdn.pika.dev/fp-ts@^2.4.4/function";
+import { Magma } from "https://cdn.pika.dev/fp-ts@^2.4.4/Magma";
+import { map } from "https://cdn.pika.dev/fp-ts@^2.4.4/Array";
 
-console.log("rendering..");
-console.log("blala..");
-console.log(typeof either);
-console.log(typeof lodash);
+
+
+
+const output = pipe(
+  'Send in the clown',
+  console.log
+)
+
 
 serve(async (req) => {
-  return new Response("Not HHHHHH", { status: 404 });
+  return new Response("TEST", { status: 200 });
 });
