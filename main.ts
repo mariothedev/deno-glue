3676ce429bb5c54fe713f21b21d2ca2a1dd30347
mariothedev <mariothedev@gmail.com>
2022-10-21 15:42:16 +0200

    First func

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 // @ts-nocheck
 import file from "./data.json" assert { type: "json"}
+import * as S from "https://cdn.pika.dev/fp-ts@^2.4.4/string";
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { pipe } from "https://cdn.pika.dev/fp-ts@^2.4.4/function";
-import { Magma } from "https://cdn.pika.dev/fp-ts@^2.4.4/Magma";
 import { map } from "https://cdn.pika.dev/fp-ts@^2.4.4/Array";
 
 
+const f = (item): any => item.text
 
+const replace = S.replace(/love/g, '')
 
-const output = pipe(
-  'Send in the clown',
-  console.log
-)
+const sentences = pipe(file.sentences, map(f))
+const removeLove = (word: any) => pipe(word, replace)
+const output =  pipe(sentences, map(removeLove) )
 
 
 serve(async (req) => {
-  return new Response("TEST", { status: 200 });
+  return new Response(output, { status: 200 });
 });
