fdff2dde129a6b530ab1515ebe6ccb4732043180
mariothedev <mariothedev@gmail.com>
2022-10-24 09:16:59 +0200

    Add kahins changes

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 import file from "./data.json" assert { type: "json"}
 import * as S from "https://cdn.pika.dev/fp-ts@^2.4.4/string";
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import { pipe } from "https://cdn.pika.dev/fp-ts@^2.4.4/function";
+import { pipe, flow } from "https://cdn.pika.dev/fp-ts@^2.4.4/function";
 import { map } from "https://cdn.pika.dev/fp-ts@^2.4.4/Array";
 
 
@@ -15,6 +15,22 @@ const removeLove = (word: any) => pipe(word, replace)
 const output =  pipe(sentences, map(removeLove) )
 
 
+
+const comp = flow(f, replace)
+const r = pipe (
+  file.sentences,
+  map(comp)
+)
+
+console.log(r)
+
 serve(async (req) => {
   return new Response(output, { status: 200 });
 });
+
+// const r = pipe (
+//     file.sentences,
+//     map(f),
+//     map(replace)
+//     map(removeI)
+//   )
